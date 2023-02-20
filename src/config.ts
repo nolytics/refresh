822ac339d9e7483f7382d143d7d295547aeddd10
freitzzz <1160907@isep.ipp.pt>
2023-02-20 17:49:19 +0000

    include environment variables for github api configuration

--- src/config.ts
+++ src/config.ts
@@ -3,6 +3,11 @@ export interface Env {
     MONGODB_APP_ID: string;
     MONGODB_DATA_SOURCE: string;
     MONGODB_DATABASE: string;
+    GITHUB_OWNER: string;
+    GITHUB_REPOSITORY_NAME: string;
+    GITHUB_REPOSITORY_BRANCH: string;
+    GITHUB_NOLYTICS_JSON_RELATIVE_FILE_PATH: string;
+    GITHUB_PERSONAL_ACCESS_TOKEN: string;
 }
 
 export interface Atlas {
@@ -12,11 +17,24 @@ export interface Atlas {
     database: string;
 }
 
-export function fromEnv(env: Env): Atlas {
+export interface GitHub {
+    owner: string;
+    repositoryName: string;
+    repositoryBranch: string;
+    nolyticsJsonRelativeFilePath: string;
+    personalAccessToken: string;
+}
+
+export function fromEnv(env: Env): Atlas & GitHub {
     return {
         apiKey: env.MONGODB_API_KEY,
         appId: env.MONGODB_APP_ID,
         database: env.MONGODB_DATABASE,
         dataSource: env.MONGODB_DATA_SOURCE,
+        nolyticsJsonRelativeFilePath: env.GITHUB_NOLYTICS_JSON_RELATIVE_FILE_PATH,
+        owner: env.GITHUB_OWNER,
+        personalAccessToken: env.GITHUB_PERSONAL_ACCESS_TOKEN,
+        repositoryName: env.GITHUB_REPOSITORY_NAME,
+        repositoryBranch: env.GITHUB_REPOSITORY_BRANCH,
     }
 }
